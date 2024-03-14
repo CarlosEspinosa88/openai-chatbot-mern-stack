a4d634ffdc1130574cb2cccfaa7ffd8ce9a7f574
Carlos Espinosa <carloespinosa88@gmail.com>
2024-03-14 16:04:29 -0500

    Add mongoDB config and servers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,39 @@
-import express from "express";
+import app from "./app.js"
+import { config } from "dotenv"
+import { connectToDataBase } from "./db/connection.js"
 
-const app = express();
+config({ path: './.env'})
+const PORT = process.env.PORT || 8080
+
+connectToDataBase().then(() => {
+  // connections and listeners
+  app.listen(PORT, () => 
+    console.log('Server open and connected to database 🤓')
+  )
+
+}).catch((error) => console.log(error))
+
 
 // GET
-app.get('/', function (req, res) {
-    // console.log(req.body)
-  return res.send('hello world')
-})
+// app.get('/', function (req, res) {
+//   return res.send('hello world')
+// })
 
 // POST
 // app.post('/send', function (req, res) {
-//   console.log(req.body)
+//   console.log(req.body.name)
 //   return res.send('send data')
 // })
 
 // PUT
 // app.put('/edit', function (req, res) {
-//   console.log(req.body)
+//   console.log(req.body.name)
 //   return res.send('edit data')
 // })
 
 // DELETE
-// app.use(express.json())
-
 // app.delete('/user/:userId', function (req, res, next) {
 //   console.log(req.params.userId)
 //   return res.send('remove data')
 // })
 
-app.listen(8000, () => console.log('Open server: port 8000'))
